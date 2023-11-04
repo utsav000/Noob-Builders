df42f567d4365fe7706d1aa7ad6dc089884102ef
Kanishk-Dukia <woekspace@gmail.com>
2023-11-04 19:15:44 +0530

    kan 19:15

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -30,7 +30,6 @@ module.exports.createCampground = async (req, res, next) => {
     // console.log(campground);
     req.flash('success', 'Successfully made a new campground');
     res.redirect(`/campgrounds/${campground._id}`);
-
 }
 
 module.exports.showCampground = async (req, res,) => {

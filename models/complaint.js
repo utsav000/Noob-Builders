d5e61799f62a4ce408b8c42dfabc99244a97e7e5
Ayushi Mohta <ayushimohta@Ayushis-MacBook-Air.local>
2023-11-05 02:02:45 +0530

    latest

--- models/complaint.js
+++ models/complaint.js
@@ -1,12 +1,21 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_300');
+});
 
 
 const complaintSchema = new Schema({
     name: String,
     email: String,
     complaint: String,
+  
     geometry: {
         type: {
             type: String,
@@ -18,8 +27,10 @@ const complaintSchema = new Schema({
             type: [Number],
             required: true
         }
-    }
-
+    },
+    phone:String,
+    category:String,
+    images: [ImageSchema]
 });
 
 module.exports = mongoose.model('Complaint', complaintSchema);
